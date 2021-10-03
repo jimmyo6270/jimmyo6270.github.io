942000123bbf10f0d74b08d41d581ffd9e8f7a94
jimmyo6270 <91750587+jimmyo6270@users.noreply.github.com>
2021-10-03 16:53:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Jimmy Or"; // update the part BETWEEN the "quotes"
+let userName = "jimmyo6270"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
